ceaf45f081916d3c2c82386d37bc4cfcf72f2970
Slingreen <benjamindarici@hotmail.com>
2021-03-09 11:40:07 +0100

    Failed generation

--- GroupProject/Intermediate/Build/Win64/GroupProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.28.29336
-#include "C:/Users/benja/Documents/GitHub/Project/GroupProject/Intermediate/Build/Win64/GroupProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- GroupProject/Source/GroupProject/GroupProject.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "GroupProject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GroupProject, "GroupProject" );
