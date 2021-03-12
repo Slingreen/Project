d22140408d5480141fb968127f6abcf59147cdeb
Fluglglugly <70482373+Fluglglugly@users.noreply.github.com>
2021-03-12 16:30:49 +0100

    Sentry added
    Added Sentry character, and included AIModule to the project to allow for UPawnSensingComponent

---
+++ Source/Project/Sentry.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Sentry.h"
+#include "Perception/PawnSensingComponent.h"
+
+// Sets default values
+ASentry::ASentry()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	//Used this page to figure out how UPawnSensingComponent works: https://forums.unrealengine.com/development-discussion/c-gameplay-programming/19571-mini-tutorial-using-upawnsensingcomponent-in-c
+	Sensing = CreateDefaultSubobject<UPawnSensingComponent>(TEXT("Sensing"));
+	Sensing->bEnableSensingUpdates=true;
+	Sensing->bOnlySensePlayers = true;
+	//these are temporary values, change in gametesting
+	Sensing->SensingInterval = .25f;
+	Sensing->SetPeripheralVisionAngle(10.f);
+
+
+}
+
+// Called when the game starts or when spawned
+void ASentry::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASentry::OnSeePlayer(ACharacter* player)
+{
+}
+
+// Called every frame
+void ASentry::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ASentry::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/Project/Sentry.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Perception/PawnSensingComponent.h"
+#include "Sentry.generated.h"
+
+UCLASS()
+class PROJECT_API ASentry : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ASentry();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+		UPROPERTY(VisibleAnywhere, Category = "AI");
+		class UPawnSensingComponent* Sensing;
+
+		UFUNCTION()
+			void OnSeePlayer(ACharacter* player);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
