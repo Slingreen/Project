acc10882d228894a4e5eb605d5c09ba0be95c7a8
Fluglglugly <70482373+Fluglglugly@users.noreply.github.com>
2021-04-09 17:00:40 +0200

    Committing som AI stuff
    Committing som AI stuff

---
+++ Source/Project/CrabMeleeTask.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CrabMeleeTask.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AICrabController.h"
+#include "CrabMonster.h"
+
+EBTNodeResult::Type UCrabMeleeTask::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+
+	AAICrabController* AICon = Cast<AAICrabController>(OwnerComp.GetAIOwner());
+	ACrabmonster* Crab = Cast<ACrabmonster>(AICon->GetPawn());
+	if (AICon)
+	{
+	
+		Crab->MeleeAttack();
+
+		return EBTNodeResult::Succeeded;
+	}
+	else {
+		return EBTNodeResult::Failed;
+	}
+}
