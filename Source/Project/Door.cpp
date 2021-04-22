3b8f45cc167f992c035db4490c87f086c1dbf895
lenijer <70570095+lenijer@users.noreply.github.com>
2021-04-22 13:49:34 +0200

    umm

--- Source/Project/Door.cpp
+++ Source/Project/Door.cpp
@@ -2,6 +2,12 @@
 
 
 #include "Door.h"
+<<<<<<< Updated upstream
+=======
+#include "Components/BoxComponent.h"
+#include "PlayerWilliam.h"
+#include "Misc/App.h"
+>>>>>>> Stashed changes
 
 // Sets default values
 ADoor::ADoor()
@@ -9,8 +15,18 @@ ADoor::ADoor()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+<<<<<<< Updated upstream
 	Collider = CreateDefaultSubobject<UBoxComponent>(TEXT("DoorCollider"));
 	RootComponent = Collider;
+=======
+	//Mesh
+	Door_Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Door_Mesh"));
+	RootComponent = Door_Mesh;
+
+	//Collider
+	Collider = CreateDefaultSubobject<UBoxComponent>(TEXT("MyCollider"));
+	Collider->SetupAttachment(RootComponent);
+>>>>>>> Stashed changes
 
 	Collider->SetGenerateOverlapEvents(true);
 
@@ -20,6 +36,11 @@ ADoor::ADoor()
 void ADoor::BeginPlay()
 {
 	Super::BeginPlay();
+<<<<<<< Updated upstream
+=======
+
+	Cast<UBoxComponent>(Collider)->OnComponentBeginOverlap.AddDynamic(this, &ADoor::Collision);
+>>>>>>> Stashed changes
 	
 }
 
@@ -27,6 +48,30 @@ void ADoor::BeginPlay()
 void ADoor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+<<<<<<< Updated upstream
 
+=======
+	if (Open == true) {
+		Rotate();
+	}
+}
+
+void ADoor::Collision(UPrimitiveComponent* OverlappedComponent, AActor* Colliding_obj, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweeb, const FHitResult& SweepResult) {
+	if (Colliding_obj->IsA(APlayerWilliam::StaticClass())) {
+		Open = true;
+	}
+}
+
+void ADoor::Rotate() {
+	if (Current_Degree >= Max_Degrees) {
+		Open = false;
+		return;
+	}
+	Current_Degree++;
+
+	FRotator Rotation = this->GetActorRotation();
+	Rotation.Yaw += Current_Degree;
+	SetActorRotation(Rotation);
+>>>>>>> Stashed changes
 }
 
--- Source/Project/Door.h
+++ Source/Project/Door.h
@@ -18,15 +18,21 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+<<<<<<< Updated upstream
 
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+=======
+	
+	//Collider
+>>>>>>> Stashed changes
 	UPROPERTY(EditAnywhere, Category = "Basic")
 		class UBoxComponent* Collider{ nullptr };
 
 	UFUNCTION()
+<<<<<<< Updated upstream
 		void Collision(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 			UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweeb,
 			const FHitResult& SweepResult);
@@ -35,4 +41,27 @@ public:
 	UPROPERTY(EditAnywhere, Category = "Basic")
 		float RotTime{ 9.f };
 
+=======
+		void Collision(UPrimitiveComponent* OverlappedComponent, AActor* Colliding_obj,
+			UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweeb,
+			const FHitResult& SweepResult);
+
+	//Mesh
+	UPROPERTY(EditAnywhere, Category = "Mesh");
+	UStaticMeshComponent* Door_Mesh{ nullptr };
+
+	//Timer
+	UPROPERTY(EditAnywhere, Category = "Rotation")
+		float RotationTimer{ 4.f };
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	bool Open{ false };
+	int Max_Degrees{ 90 };
+	int Current_Degree{ 0 };
+	void Rotate();
+>>>>>>> Stashed changes
 };
